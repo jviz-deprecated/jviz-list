4b218d8d2031d41544b7d134fb2666ca30ee2424
jmjuanes <josemijuanes@gmail.com>
2016-09-01 18:01:31 +0200

    js/data.js: major update

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
 //Display the provided data
 jviz.modules.editableList.prototype.data = function(data)
 {
+  //Check for undefined data
+  if(typeof data === 'undefined'){ return this._dats.src; }
+
   //Check the data
   if(jviz.is.array(data) === false){ data = [ data ]; }
-
-  //Save this
-  var self = this;
 
   //Reset the container
   jviz.dom.html('', this._table.id);
@@ -14,18 +14,35 @@ jviz.modules.editableList.prototype.data = function(data)
   this._data.active = Array.apply(null, Array(this._data.src.length)).map(function(v){ return true; });
 
   //Display all data
-  data.forEach(function(el, index)
+  for(var i = 0; i < data.length; i++)
   {
     //Get the id
-    var id = self._row.id + '-' + index;
+    var id = this._row.id + '-' + i;
 
     //Add the row
-    jviz.dom.append({ _tag: 'div', id: id, class: self._row.class }, self._table.id);
+    jviz.dom.append({ _tag: 'div', id: id, class: this._row.class }, this._table.id);
 
     //Display the new element
-    self.display(el, index, false);
-  });
+    this.display(data[i], i, false);
+  }
+
+  //Add the events
+  for(var i = 0; i < data.length; i++)
+  {
+    //Add the events
+    this.btnEvents(i, false);
+  }
 
   //Save the data
   this._data.src = data;
+};
+
+//Get a data element
+jviz.modules.editableList.prototype.element = function(index)
+{
+  //Check the index
+  if(typeof index === 'undefined'){ return this._data.src; }
+
+  //Return the data index
+  return this._data.src[index];
 };
