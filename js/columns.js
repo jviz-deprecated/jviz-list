7e756a255930a28398315fcf042be9b07fe981b7
jmjuanes <josemijuanes@gmail.com>
2016-09-01 18:00:38 +0200

    js/columns.js: parse columns options

--- js/columns.js
+++ js/columns.js
@@ -7,8 +7,22 @@ jviz.modules.editableList.prototype.columns = function(list)
   //Parse all the columns
   list.map(function(el, index)
   {
+    //Check the key
+    if(typeof el.key === 'undefined'){ throw new Error('No key on column ' + index); } 
+    //Inpiut type
+    if(typeof el.type === 'undefined'){ el.type = 'text'; }
+
+    //Options
+    if(typeof el.options === 'undefined'){ el.options = []; }
+
     //Check for editable
     if(typeof el.editable === 'undefined'){ el.editable = true; }
+
+    //Check the detail function/string
+    if(typeof el.detail === 'undefined'){ el.detail = ''; }
+
+    //Check the column input helper
+    if(typeof el.helper === 'undefined'){ el.helper = ''; }
 
     //Return the element
     return el;
