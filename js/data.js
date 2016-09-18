f6e0fc36f54492b34694f3129e1db96395319624
jmjuanes <josemijuanes@gmail.com>
2016-09-18 13:50:01 +0200

    js/data.js: big rewrite

--- js/data.js
+++ js/data.js
@@ -2,43 +2,20 @@
 jviz.modules.editableList.prototype.data = function(data)
 {
   //Check for undefined data
-  if(typeof data === 'undefined'){ return this._dats.src; }
+  if(typeof data === 'undefined'){ var data = this._data.src; }
 
   //Check the data
   if(jviz.is.array(data) === false){ data = [ data ]; }
 
-  //Reset the container
-  jviz.dom.html(this._table.id, '');
-
-  //Initialize the active counter
-  this._data.active = Array.apply(null, Array(this._data.src.length)).map(function(v){ return true; });
-
-  //Display all data
-  for(var i = 0; i < data.length; i++)
-  {
-    //Get the id
-    var id = this._row.id + '-' + i;
-
-    //Add the row
-    jviz.dom.append(this._table.id, { _tag: 'div', id: id, class: this._row.class });
-
-    //Display the new element
-    this.display(data[i], i, false);
-  }
-
-  //Add the events
-  for(var i = 0; i < data.length; i++)
-  {
-    //Add the events
-    this.btnEvents(i, false);
-  }
-
   //Save the data
   this._data.src = data;
+
+  //Return this
+  return this;
 };
 
-//Get a data element
-jviz.modules.editableList.prototype.element = function(index)
+//Get the actual data
+jviz.modules.editableList.prototype.getData = function(index)
 {
   //Check the index
   if(typeof index === 'undefined'){ return this._data.src; }
