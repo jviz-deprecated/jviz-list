b2210d3c7ab89e522ea36d1ca2d4ec89bd7a0082
jmjuanes <josemijuanes@gmail.com>
2016-10-24 00:12:32 +0200

    js/action-save.js: minor update

--- js/action-save.js
+++ js/action-save.js
@@ -8,7 +8,7 @@ jviz.modules.editableList.prototype.save = function(index, obj)
   if(typeof obj === 'undefined'){ return; }
 
   //Save the element of the list
-  this._data.src[index] = jviz.misc.extend(this._data.src[index], obj);
+  this._data.src[index] = jviz.object.extend(this._data.src[index], obj);
 
   //Update the element
   this.display(this._data.src[index], index, false);
